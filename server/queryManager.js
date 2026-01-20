d49344136cffe65dbad2320556bf9a0497ba6692
Armaan Amoli <armaanmohanamoli@gmail.com>
2026-01-20 12:45:17 +0000

    Before JWT auth

--- server/queryManager.js
+++ server/queryManager.js
@@ -1,10 +1,28 @@
 import mongoose from "mongoose";
 import process from 'node:process';
 import {User, Portfolio, Transaction} from "./mongoSchema.js"
+import bcrypt from "bcrypt";
 
 mongoose.connect(String(process.env.MONGO_URL)).then(()=>{
-    console.log("CONNECTED")
-    process.exit(0);
+    console.log("CONNECTED");
 }).catch(err =>{
     console.log(err);
 })
+
+export async function SignUp(details){
+    const {email , password , username} = details;
+    const passwordHash = await bcrypt.hash(password, 10); //SaltRounds = 10
+    const newUser = new User({username:username , email:email , passwordHash:passwordHash, createdAt:Date.now(), lastLogin:Date.now()});
+    await newUser.save();
+}
+
+export async function login(details){
+    const {email , password} = details;
+    const userData = await User.findOne({email:email});
+    if(!userData) return null;
+    const match = await bcrypt.compare(password , userData.passwordHash);
+    if(!match){
+        return null;
+    }
+    return userData; 
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express, { application } from 'express';
 import mongoose from 'mongoose';
 import process from 'node:process';
+import { SignUp, login } from './queryManager.js';
+
 const server = express();
 const port = 3000;
 
@@ -8,18 +10,48 @@ await mongoose.connect(String(process.env.MONGO_URL));
 
 application.use(express.json());
 const validateEmail = (req,res,next)=>{
-    const {username , email , password} = req.body;
-    
-    return;
+    const {email} = req.body;
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(pattern.test(email)){
+        return next();
+    }
+    else{
+        res.status(400).json({message:"Invalid email"});
+        return;
+    }
+
 }
 
-server.post('/sign-up', validateEmail , (req,res,next)=>{
+server.post('/sign-up', validateEmail , async(req,res,next)=>{
     try{
-        console.log('New entry');
+        await SignUp(req.body);
+        res.status(200).json({message:"User Registered"});
     }catch(err){
         next(err);
     }
-})
+});
+
+server.post('/login', validateEmail , async(req,res,next)=>{
+    try{
+        const isValidlogin = await login(req.body); 
+        if(isValidlogin){
+            res.status(200).json({message:"Login Success"});
+            return;
+        }
+        else{
+            res.status(401).json({message:"Incorrect Password"});
+        }
+    }catch(err){
+        next(err);
+    }
+});
+
+// Global Error Handler Middleware
+server.use((err,res) => {
+    console.error(`Error Stack: ${err.stack}`);
+    const statusCode = err.status || 500; // If error already have a status code if not give it 500(Server Error)
+    res.status(statusCode).send("Server Error");
+});
 
 server.listen(port, (error)=>{
     if(!error){
