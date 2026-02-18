856b1b90ca47c60af8c35c8c83c77011754b29c8
Armaan Amoli <armaanmohanamoli@gmail.com>
2026-02-18 14:25:07 +0000

    portfolio updater - i

--- src/pages/portfolioPage.jsx
+++ src/pages/portfolioPage.jsx
@@ -3,9 +3,12 @@ import { useEffect } from "react";
 import { getQuote } from "../../server/getQuote";
 import Navbar from "./navbar";
 import axios from "axios";
+import { data } from "react-router-dom";
+import { fetchQuote } from "./Charts/dataRequester";
 
 export default function PortfolioPage() {
-    const [assetList, setAssetList] = useState({});
+    const [assetList, setAssetList] = useState([]);
+    const [prices , setPrices] = useState({});
     const token = localStorage.getItem('token');
     useEffect(() => {
         async function getPortfolio() {
@@ -17,15 +20,29 @@ export default function PortfolioPage() {
                 }
                 )
                 console.log(portfolio.data.positions);
-                return portfolio.data;
+                setAssetList(portfolio.data.positions);
+                return
             }
             catch (err) {
                 console.log(err);
             }
         }
         getPortfolio();
+    }, [token]);
 
-        //cosnt currentPrice = await getQuote
+    useEffect(()=>{
+        if(assetList.length === 0) return;
+        async function updatePrices(){
+            const updatedData = {};
+            for(const item of assetList){
+                try{
+                    const quote = await getQuote(item.symbol);
+                    updatedData[item.symbol] = {Pnl: quote.currentPrice-item.avgPrice};
+                }catch(e){console.log(e)};
+            }
+            setPrices(prev=>({...prev , ...updatePrices}));
+        };
+        updatePrices();
     })
 
     return (
@@ -45,7 +62,23 @@ export default function PortfolioPage() {
                             </tr>
                         </thead>
                         <tbody>
-
+                            {assetList.map((item, index) => (
+                                <tr key={item.symbol}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.symbol}</td>
+                                    <td>{item.shares}</td>
+                                    <td>{setInterval(
+                                        async () => {
+                                            const { currentPrice } = getQuote(item.symbol);
+                                            return currentPrice;
+                                        }, 5000)}</td>
+                                    <td>{setInterval(
+                                        async () => {
+                                            const { percentChange } = getQuote(item.symbol);
+                                            return percentChange;
+                                        }, 5000)}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
