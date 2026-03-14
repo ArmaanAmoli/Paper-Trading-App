ba95ac6fb77a37b42304c4f9acf3a0b7b8ed03ed
Armaan Amoli <armaanmohanamoli@gmail.com>
2026-03-14 13:38:39 +0000

    trying to make search pop up tab hover commit

---
+++ src/pages/floatingSearchTab.jsx
@@ -0,0 +1,5 @@
+export default function SearchTabPopUp(){
+    return(
+        <div className="flex flex-col basis-md h-96 bg-black rounded-4xl border border-gray-700"></div>
+    );
+}
--- src/pages/watchlist.jsx
+++ src/pages/watchlist.jsx
@@ -1,23 +1,37 @@
 import Ticker from "./ticker.jsx";
 import "./styles/watchlist.css";
+import { useState } from "react";
+import SearchTabPopUp from "./floatingSearchTab.jsx";
+import { createPortal } from "react-dom";
 export default function Watchlist() {
+    const [searchTabOpen, setSearchTabOpen] = useState(false)
+
+    const toggleSearchTab = () => {
+        setSearchTabOpen(prev => !prev)
+    }
     return (
-        <div className="stock-list">
-            <div className="py-10px flex flex-row justify-between w-full">
-                <h4>Watchlist</h4>
-                <button className="pb-10 w-[30px] h-[30px] border-1 flex justify-center items-center rounded-full text-xl hover:bg-[#666666]">+</button>
-            </div>
+        <div className="relative">
+            {
+                searchTabOpen && (createPortal(<SearchTabPopUp className="absolute z-1000 top-0"/> , document.body))
+            }
+            <div className="stock-list">
+                <div className="py-10px flex flex-row justify-between w-full">
+                    <h4>Watchlist</h4>
+                    <button className="pb-10 w-[30px] h-[30px] border-1 flex justify-center items-center rounded-full text-xl hover:bg-[#666666] " onClick={toggleSearchTab}>+</button>
+                </div>
 
-            <div className="heading-wl">
-                <p>Symbol</p>
-                <p>Price</p>
-                <p>Change</p>
-                <p>%Change</p>
-            </div>
+                <div className="heading-wl">
+                    <p>Symbol</p>
+                    <p>Price</p>
+                    <p>Change</p>
+                    <p>%Change</p>
+                </div>
 
-            <Ticker name="RR.L" />
-            <Ticker name="^FTSE" />
-            <Ticker name="LLY" />
+                <Ticker name="RR.L" />
+                <Ticker name="^FTSE" />
+                <Ticker name="LLY" />
+            </div>
         </div>
+
     );
 }
